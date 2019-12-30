bbdb5e6001164f68c508867d96a211a90c88fe74
Sampat Ponnaganti <sampat.p@gmail.com>
2019-12-30 11:54:21 -0500

    Added class based and function based views

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -7,4 +7,17 @@ urlpatterns = [
     path('post_test', views.post_test, name='post_test'),
     path('add_post', views.add_post, name='add_post'),
     path('static_test', views.static_test, name='post_test'),
+
+    # Class based views
+    path('destinations', views.DestinationList.as_view(), name='destination_list'),
+    path('destinations/<int:pk>', views.DestinationDetail.as_view(), name='destination_detail'),
+    path('create', views.DestinationCreate.as_view(), name='destination_create'),
+    path('update/<int:pk>', views.DestinationUpdate.as_view(), name='destination_update'),
+    path('delete/<int:pk>', views.DestinationDelete.as_view(), name='destination_delete'),
+
+    # Function based views
+    path('destinationslist', views.Destination_list, name='destination_list1'),
+    path('destinationscreate', views.Destination_create, name='destination_new1'),
+    path('destinationsedit/<int:pk>', views.Destination_update, name='destination_edit1'),
+    path('destinationsdelete/<int:pk>', views.Destination_delete, name='destination_delete1'),
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
+from .models import Destination
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.forms import ModelForm
+from django.shortcuts import render, redirect, get_object_or_404
+
 # Create your views here.
 
 def index(request):
@@ -18,3 +25,57 @@ def add_post(request):
 def static_test(request):
     dests = models.Destination.objects.all()
     return render(request, "travello_index.html", { 'dests': dests})
+
+class DestinationList(ListView):
+    model = models.Destination
+
+class DestinationDetail(DetailView):
+    model = models.Destination
+
+class DestinationCreate(CreateView):
+    model = models.Destination
+    fields = ['name', 'desc', 'price']
+    success_url = reverse_lazy('destination_list')
+
+class DestinationUpdate(UpdateView):
+    model = models.Destination
+    fields = ['name', 'desc']
+    success_url = reverse_lazy('destination_list')
+
+class DestinationDelete(DeleteView):
+    model = models.Destination
+    fields = ['name', 'desc']
+    success_url = reverse_lazy('destination_list')
+
+class DestinationForm(ModelForm):
+    class Meta:
+        model = models.Destination
+        fields = ['name', 'desc']
+
+def Destination_list(request, template_name='first_app/destination_list.html'):
+    Destination = models.Destination.objects.all()
+    data = {}
+    data['object_list'] = Destination
+    return render(request, template_name, data)
+
+def Destination_create(request, template_name='first_app/destination_form.html'):
+    form = DestinationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('destination_list1')
+    return render(request, template_name, {'form':form})
+
+def Destination_update(request, pk, template_name='first_app/destination_form.html'):
+    Destination= get_object_or_404(models.Destination, pk=pk)
+    form = DestinationForm(request.POST or None, instance=Destination)
+    if form.is_valid():
+        form.save()
+        return redirect('destination_list1')
+    return render(request, template_name, {'form':form})
+
+def Destination_delete(request, pk, template_name='first_app/destination_confirm_delete.html'):
+    Destination= get_object_or_404(models.Destination, pk=pk)
+    if request.method=='POST':
+        Destination.delete()
+        return redirect('destination_list1')
+    return render(request, template_name, {'object':Destination})
